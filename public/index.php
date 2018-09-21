2a72a2e7880e0163f5a9c47b196734a3c85d0290
Руслан Хорошкевич <russocomp.zezst@gmail.com>
2018-09-21 15:51:47 +0300

    tmp

--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (is_readable(__DIR__.'/../function.php')) {
-  require_once __DIR__.'/../function.php';
-}
-
-//далее подключаем ваш проект
-vdump(__DIR__);
-vdump($_POST, $_GET, 'да вообще что угодно', new Exception(), ['sasa',5,6=>'six','ping'=>'понг']);
